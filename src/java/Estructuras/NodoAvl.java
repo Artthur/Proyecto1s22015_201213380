6655d606ea99419f7b865d7761c978f908f4c81b
Arthur <ramirez14usac@gmail.com>
2015-09-13 11:06:18 -0600

    Update NodoAvl.java

--- src/java/Estructuras/NodoAvl.java
+++ src/java/Estructuras/NodoAvl.java
@@ -3,11 +3,14 @@ package estructuras;
 public class NodoAvl{
 
 int id, fe;
+String correo,contraseña;
 NodoAvl nodoIzquierdo,nodoDerecho;
 
 
-public NodoAvl(int id){
+public NodoAvl(int id,String correo,String contraseña){
 
+this.correo = correo;
+this.contraseña = contraseña;
 this.id = id;
 this.fe=0;
 this.nodoIzquierdo=null;
